d570e9ceb828a3384ac872d216710d989be5bfa6
AJV304 <100213034+AJV304@users.noreply.github.com>
2026-01-05 17:54:43 +0100

    Adding a table

--- 03_reusable_code/ShinyAnna/app.R
+++ 03_reusable_code/ShinyAnna/app.R
@@ -4,7 +4,7 @@ library(shiny)
 # create user interface
 ui <- fluidPage(
     #title
-    titlePanel("Anna's Shiny App"),
+    titlePanel("Anna's Shiny App about The Titanic!"),
 
     #how to display input and output
     sidebarLayout(
@@ -12,9 +12,11 @@ ui <- fluidPage(
       sidebarPanel(sliderInput("age", "Age",
                                min = 0, max = 90, value = c(20, 50), post = " years"),
                    selectInput("class", "Travel class", choices = c("1", "2", "3"), multiple = TRUE, selected = c("1", "2", "3")),
-                   checkboxInput("survival", "Show survival?", value = FALSE)),
+                   checkboxInput("survival", "Group based on survival?", value = FALSE)),
       #output
-      mainPanel("[plot placeholder]", plotOutput("scatterplot"),
+      mainPanel("On this website you can explore data from the famously crashed Titanic. The graph below allows you to explore the relationship between the age of the passengers and how much they paid for a ticket while playing with additional variables.", 
+                br(), br(), 
+                plotOutput("scatterplot"),
                 br(),
                 br(),
                 "[table placeholder]", tableOutput("descriptives"))
@@ -26,12 +28,16 @@ ui <- fluidPage(
 #what is happening in the background
 server <- function(input, output){
 
+  
   output$scatterplot <- renderPlot({
-    
     filtered <- data3 %>% 
       filter( Pclass %in% input$class,
               Age >= input$age[1],
               Age <= input$age[2])
+    filtered$Survived <- factor(filtered$Survived,
+                                levels = c(0, 1),
+                                labels = c("Did Not Survive", "Survived"))
+    
     
     # Build the aesthetic mapping dynamically
     mapping <- aes(x = Age, y = Fare)
@@ -40,12 +46,29 @@ server <- function(input, output){
     ifelse(input$survival == TRUE, mapping <- aes(x = Age, y = Fare, color = Survived), mapping <- mapping) 
     
   ggplot(filtered, mapping) +
-    geom_point() +
+    geom_point(shape = 20, size = 2) +
+    scale_color_manual(values = c("Did Not Survive" = "black", "Survived" = "#1b98E099")) +
+    labs(color = "Survival Status") +
     geom_smooth(method = "lm", se = FALSE) +
-    theme_minimal() +
-    labs(title = paste("Fare per Age from ages", input$age[1], "to", input$age[2], "in travel class", paste(input$class, collapse = ", ")))} 
+    theme_light() +
+    labs(title = paste("Fare per age from ages", input$age[1], "to", input$age[2], "in travel class", paste(input$class, collapse = ", "))) +
+    theme(
+      plot.title = element_text(hjust = 0.5, size = 16)
+    )} )
   
-  )}
+  output$descriptives <- renderTable({
+    filtered <- data3 %>% 
+      filter( Pclass %in% input$class,
+              Age >= input$age[1],
+              Age <= input$age[2])
+    
+    filtered %>% group_by(Sex) %>% 
+      summarise("mean survival rate" = mean(Survived, na.rm = TRUE),
+                "mean fare" = mean(Fare, na.rm = TRUE),
+                "mean age" = mean(Age, na.rm = TRUE))
+  })
+  
+  }
 
 #tell shiny to combine the 2 
 shinyApp(ui = ui, server = server)
