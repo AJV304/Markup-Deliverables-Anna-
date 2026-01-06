c590edb71e30756be7bbeaa1bfe85599f9ae229f
AJV304 <100213034+AJV304@users.noreply.github.com>
2026-01-06 16:13:16 +0100

    create image folder, add image and information to top of page, add data set to folder

--- 03_reusable_code/ShinyAnna/app.R
+++ 03_reusable_code/ShinyAnna/app.R
@@ -1,26 +1,50 @@
 #pacakge
 library(shiny)
+library(dplyr)
+library(ggplot2)
+
+data3 <- read.csv("titanic.csv")
 
 # create user interface
 ui <- fluidPage(
     #title
     titlePanel("Anna's Shiny App about The Titanic!"),
+    
+    br(),
+    
+    fluidRow(
+      column(7,
+            img(src = "titanic.jpg", style = "width:100%; height:auto;")),
+      column(5,
+             style = "font-size:18px;",
+             "On this website you can explore data from the famous ship Titanic. The Titanic sank in 1912, after crashing into an iceberg. It is estimated that over 2/3 of the passengers aboard the ship died. This tragedy drew lots of attention and remains a common pop-culture reference to this day.",
+             br(),
+             br(),
+             "This site allows you to explore data from the passengers of the Titanic such as their age, travel class, fare, and whether they survived.
+             The graph below invites you to look at the relationship between the age of the passengers and how much they paid for a ticket while playing with additional variables."
+             ) 
+    ),
+    
+    br(),
+    br(),
 
     #how to display input and output
     sidebarLayout(
       #input
-      sidebarPanel(sliderInput("age", "Age",
-                               min = 0, max = 90, value = c(20, 50), post = " years"),
+      sidebarPanel(sliderInput("age", "Age", min = 0, max = 90, value = c(20, 50), post = " years"),
                    selectInput("class", "Travel class", choices = c("1", "2", "3"), multiple = TRUE, selected = c("1", "2", "3")),
                    checkboxInput("survival", "Group based on survival?", value = FALSE)),
       #output
-      mainPanel("On this website you can explore data from the famously crashed Titanic. The graph below allows you to explore the relationship between the age of the passengers and how much they paid for a ticket while playing with additional variables.", 
+      mainPanel(
                 br(), br(), 
                 plotOutput("scatterplot"),
                 br(),
                 br(),
                 "[table placeholder]", tableOutput("descriptives"))
-    )
+    ),
+    
+    br(),
+    br()
 )
     
     
