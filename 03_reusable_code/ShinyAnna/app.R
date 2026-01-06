c6ac7e2370834ee2b4b8ee664ffeb2b9249b8640
AJV304 <100213034+AJV304@users.noreply.github.com>
2026-01-06 16:40:09 +0100

    adding hyperreferences and layout

--- 03_reusable_code/ShinyAnna/app.R
+++ 03_reusable_code/ShinyAnna/app.R
@@ -1,46 +1,64 @@
-#pacakge
+#packages
 library(shiny)
 library(dplyr)
 library(ggplot2)
 
+#load in the data
 data3 <- read.csv("titanic.csv")
 
 # create user interface
 ui <- fluidPage(
+  
     #title
     titlePanel("Anna's Shiny App about The Titanic!"),
     
     br(),
     
+    #top part with image and text
     fluidRow(
       column(7,
             img(src = "titanic.jpg", style = "width:100%; height:auto;")),
       column(5,
+             br(),
              style = "font-size:18px;",
              "On this website you can explore data from the famous ship Titanic. The Titanic sank in 1912, after crashing into an iceberg. It is estimated that over 2/3 of the passengers aboard the ship died. This tragedy drew lots of attention and remains a common pop-culture reference to this day.",
+             tags$sup(tags$a("1", href = "https://en.wikipedia.org/wiki/RMS_Titanic", target = "_blank")),
              br(),
              br(),
              "This site allows you to explore data from the passengers of the Titanic such as their age, travel class, fare, and whether they survived.
-             The graph below invites you to look at the relationship between the age of the passengers and how much they paid for a ticket while playing with additional variables."
+             The graph below invites you to look at the relationship between the age of the passengers and how much they paid for a ticket while playing with additional variables.",
+             br(),
+             br(),
+             "Data for this project was found on a",
+             tags$a("GitHub", href = "https://github.com/datasciencedojo/datasets/blob/master/titanic.csv", target = "_blank"),
+             "page promoting open data exploration. So feel free to play around and see what you can discover within this data!"
              ) 
     ),
     
+    br(),
+    br(),
     br(),
     br(),
 
     #how to display input and output
     sidebarLayout(
       #input
-      sidebarPanel(sliderInput("age", "Age", min = 0, max = 90, value = c(20, 50), post = " years"),
+      sidebarPanel(tags$style(".well {background-color:#daf4f0;}"),
+                   div(style = "font-size:18px;" , "Select your desired parameter values:"),
+                   br(),
+                   br(),
+                   sliderInput("age", "Age", min = 0, max = 90, value = c(20, 50), post = " years"),
+                   br(),
                    selectInput("class", "Travel class", choices = c("1", "2", "3"), multiple = TRUE, selected = c("1", "2", "3")),
+                   br(),
                    checkboxInput("survival", "Group based on survival?", value = FALSE)),
       #output
-      mainPanel(
-                br(), br(), 
-                plotOutput("scatterplot"),
+      mainPanel(plotOutput("scatterplot"),
                 br(),
                 br(),
-                "[table placeholder]", tableOutput("descriptives"))
+                div(
+                style = "margin-left: auto; margin-right: auto; width: fit-content;",
+                tableOutput("descriptives")))
     ),
     
     br(),
@@ -87,9 +105,9 @@ server <- function(input, output){
               Age <= input$age[2])
     
     filtered %>% group_by(Sex) %>% 
-      summarise("mean survival rate" = mean(Survived, na.rm = TRUE),
-                "mean fare" = mean(Fare, na.rm = TRUE),
-                "mean age" = mean(Age, na.rm = TRUE))
+      summarise("Mean survival rate" = mean(Survived, na.rm = TRUE),
+                "Mean fare" = mean(Fare, na.rm = TRUE),
+                "Mean age" = mean(Age, na.rm = TRUE))
   })
   
   }
