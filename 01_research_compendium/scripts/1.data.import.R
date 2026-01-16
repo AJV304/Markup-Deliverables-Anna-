d12ca3e96ed3e2b7b433e70a4d3c227caa150d8b
AJV304 <100213034+AJV304@users.noreply.github.com>
2026-01-16 15:21:13 +0100

    changing folder structure

--- 01_research_compendium/scripts/1.data.import.R
+++ 01_research_compendium/scripts/1.data.import.R
@@ -1,10 +1,10 @@
 #Raw data importation
 
-d1 <- sasxport.get("DEMO_I.xpt")
-d2 <- sasxport.get("BPX_I.xpt")
-d3 <- sasxport.get("BMX_I.xpt")
-d4 <- sasxport.get("GHB_I.xpt")
-d5 <- sasxport.get("TCHOL_I.xpt")
+d1 <- sasxport.get("data/DEMO_I.xpt")
+d2 <- sasxport.get("data/BPX_I.xpt")
+d3 <- sasxport.get("data/BMX_I.xpt")
+d4 <- sasxport.get("data/GHB_I.xpt")
+d5 <- sasxport.get("data/TCHOL_I.xpt")
 d1.t <- subset(d1,select=c("seqn","riagendr","ridageyr"))
 d2.t <- subset(d2,select=c("seqn","bpxsy1"))
 d3.t <- subset(d3,select=c("seqn","bmxbmi"))
